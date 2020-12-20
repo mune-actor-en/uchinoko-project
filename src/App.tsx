2f108dbef2a1b65ea70bdd7bea889c2692f2fb39
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-12-20 14:01:58 +0900

    サインイン画面を新規作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React,{ FC } from 'react';
-import { SignUp } from './templates/index'
+import { SignUp,SignIn } from './templates/index'
 
 const App: FC = () => {
   return (
     <div className="App" id="Uchinoko">
       <SignUp email={"foo"} password={"bar"} comfirmPassword={"bar"}/>
+      <SignIn email={"foo"} password={"bar"} comfirmPassword={"bar"} />
     </div>
   );
 }
