b9ec89ad2c7c1644899ac4924f8092eb3312d9b5
Yusuke Mabuchi <mabu.you@gmail.com>
2021-01-03 15:51:09 +0900

    Add router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React,{ FC } from 'react';
-import { SignUp, SignIn } from './templates/index'
-import { PostEdit } from './templates'
+import Router from './Router'
 
 const App: FC = () => {
   return (
     <div className="App" id="Uchinoko">
-      {/* <SignUp email={"foo"} password={"bar"} comfirmPassword={"bar"}/> */}
-      <SignIn />
-      {/* <PostEdit /> */}
+      <Router />
     </div>
   );
 }
