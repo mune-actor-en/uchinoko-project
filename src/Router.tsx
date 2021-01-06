542b2a0b2169720f43dadd7c5a0439999ca58d51
Yusuke Mabuchi <mabu.you@gmail.com>
2021-01-06 20:21:54 +0900

    Add profile router

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router'
 // templates
 import {
   PostEdit,
+  Profile,
   SignIn,
   SignUp,
 } from './templates'
@@ -15,6 +16,7 @@ const Router: FC = () => {
       <Route exact path='/signin' component={SignIn} />
       <Route exact path='/signup' component={SignUp} />
       <Route path='/post/edit(/:id)?' component={PostEdit} />
+      <Route path='/profile(/:id)?' component={Profile} />
     </Switch>
   )
 }
